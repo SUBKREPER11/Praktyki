78f262c3a621ba43dc118966b3053a7c5f7e9740
SUBKREPER11 <80513311+SUBKREPER11@users.noreply.github.com>
2022-11-18 13:13:43 +0100

    UL

--- UL/Form1.Designer.cs
+++ UL/Form1.Designer.cs
@@ -31,10 +31,10 @@ namespace UL
         {
             this.workerBeeJob = new System.Windows.Forms.ComboBox();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.shifts = new System.Windows.Forms.NumericUpDown();
             this.assignJob = new System.Windows.Forms.Button();
+            this.shifts = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.nextShift = new System.Windows.Forms.Button();
             this.report = new System.Windows.Forms.TextBox();
             this.groupBox1.SuspendLayout();
@@ -70,23 +70,15 @@ namespace UL
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Przydział prac robotnicom";
             // 
-            // label1
+            // assignJob
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(6, 16);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(92, 13);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Zadanie robotnicy";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(160, 16);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(41, 13);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "Zmiany";
+            this.assignJob.Location = new System.Drawing.Point(6, 59);
+            this.assignJob.Name = "assignJob";
+            this.assignJob.Size = new System.Drawing.Size(277, 23);
+            this.assignJob.TabIndex = 4;
+            this.assignJob.Text = "Przypisz tę pracę pszczole";
+            this.assignJob.UseVisualStyleBackColor = true;
+            this.assignJob.Click += new System.EventHandler(this.assignJob_Click);
             // 
             // shifts
             // 
@@ -105,14 +97,23 @@ namespace UL
             0,
             0});
             // 
-            // assignJob
+            // label2
             // 
-            this.assignJob.Location = new System.Drawing.Point(6, 59);
-            this.assignJob.Name = "assignJob";
-            this.assignJob.Size = new System.Drawing.Size(277, 23);
-            this.assignJob.TabIndex = 4;
-            this.assignJob.Text = "Przypisz tę pracę pszczole";
-            this.assignJob.UseVisualStyleBackColor = true;
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(160, 16);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Zmiany";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Zadanie robotnicy";
             // 
             // nextShift
             // 
@@ -123,6 +124,7 @@ namespace UL
             this.nextShift.TabIndex = 2;
             this.nextShift.Text = "Przepracuj następną zmianę";
             this.nextShift.UseVisualStyleBackColor = true;
+            this.nextShift.Click += new System.EventHandler(this.nextShift_Click);
             // 
             // report
             // 
@@ -131,7 +133,6 @@ namespace UL
             this.report.Name = "report";
             this.report.Size = new System.Drawing.Size(416, 215);
             this.report.TabIndex = 3;
-            this.report.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // Form1
             // 
@@ -141,8 +142,12 @@ namespace UL
             this.Controls.Add(this.report);
             this.Controls.Add(this.nextShift);
             this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "System zarządzania ulem";
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.shifts)).EndInit();
