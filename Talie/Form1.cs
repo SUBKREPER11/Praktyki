8b4b89c4c5660f3ef90ea400e0a69dca85f7a416
SUBKREPER11 <80513311+SUBKREPER11@users.noreply.github.com>
2022-11-21 14:44:50 +0100

    Talie (część)

---
+++ Talie/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Talie
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void moveToDeck2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void moveToDeck1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void reset1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void reset2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void shuffle1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void shuffle2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public void RedrawDeck(int DeckNumer)
+        {
+            if(DeckNumer == 1)
+            {
+                listBox1.Items.Clear();
+                foreach (string cardName in deck1.GetCardNames())
+                    listBox1.Items.Add(cardName);
+                label1.Text = "Zestaw 1. (" + deck1.Count + " kart)";
+            }
+            else
+            {
+                listBox2.Items.Clear();
+                foreach (string cardName in deck2.GetcardNames())
+                    listBox2.Items.Add(cardName);
+                label2.Text = "Zestaw 2. (" + deck2.Count + " kart)";
+            }
+        }
+    }
+    class Deck
+    {
+        private List<Card> cards;
+        private Random random = new Random();
+
+        public Deck()
+        {
+            cards = new List<Card>();
+            for (int suit = 0; suit <= 3; suit++)
+                for (int value = 1; value <= 13; value++)
+                    cards.Add(new Card((Suits)suit, (Values)value));
+        }
+        public Deck (IEnumerable<Card> initialCards)
+        {
+            cards = new List<Card>(initialCards);
+        }
+        public int Count { get { return cards.Count; } }
+        public void Add(Card cardToAdd)
+        {
+            cards.Add(cardToAdd);
+        }
+        public Card Deal(int index)
+        {
+            Card CardToDeal = cards[index];
+            cards.RemoveAt(index);
+            return CardToDeal;
+        }
+        public void Shuffle()
+        {
+            //Tasowanie kart
+        }
+        public IEnumerable<string> GetCardNames()
+        {
+            //Zwaracanie tablicy łańcuchów
+        }
+        public void Sort()
+        {
+            cards.Sort(new cardComparer_bySuit());
+        }
+    }
+}
