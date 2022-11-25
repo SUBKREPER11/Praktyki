9a45f70dfb0d57049d44e2feffecfec2de31b5f0
SUBKREPER11 <80513311+SUBKREPER11@users.noreply.github.com>
2022-11-25 13:05:33 +0100

    zdarzenia

---
+++ zdarzenia/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace zdarzenia
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Właśnie kliknąłeś formularz.");
+        }
+        private void SaySomething(object sender, EventArgs e)
+        {
+            MessageBox.Show("Coś");
+        }
+        private void SaySomethingElse(object sender, EventArgs e)
+        {
+            MessageBox.Show("Coś INNEGO!!!");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Click += new EventHandler(SaySomething);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Click += new EventHandler(SaySomethingElse);
+        }
+    }
+}
