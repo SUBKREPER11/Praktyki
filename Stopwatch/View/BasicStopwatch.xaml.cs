8188bbc1ad7f41090f72b4211b733832cf756701
SUBKREPER11 <80513311+SUBKREPER11@users.noreply.github.com>
2022-12-01 10:02:21 +0100

    Stoper (nie działa)

---
+++ Stopwatch/View/BasicStopwatch.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+//Szablon elementu Pusta strona jest udokumentowany na stronie https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Stopwatch.View
+{
+    /// <summary>
+    /// Interaction logic for BasicStopwatch.xaml
+    /// </summary>
+    public partial class BasicStopwatch : UserControl
+    {
+
+        //ViewModel.StopwatchViewModel viewModel;
+
+        public BasicStopwatch()
+        {
+            InitializeComponent();
+            viewModel = FindResource("viewModel") as ViewModel.StopwatchViewModel;
+        }
+
+        private void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            viewModel.Start();
+        }
+
+        private void ResetButton_Click(object sender, RoutedEventArgs e)
+        {
+            viewModel.Reset();
+        }
+
+        private void StopButton_Click(object sender, RoutedEventArgs e)
+        {
+            viewModel.Stop();
+        }
+
+        private void LapButton_Click(object sender, RoutedEventArgs e)
+        {
+            viewModel.Lap();
+        }
+    }
+}
