5983cc4e56307e1f57a33cad087e507feb6c3a05
SUBKREPER11 <80513311+SUBKREPER11@users.noreply.github.com>
2022-12-05 12:48:02 +0100

    zip

---
+++ ZIP/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.WindowsAPICodePack.Dialogs;
+
+namespace ZIP
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string start = @"C:\Praktyki\P_Data";
+        string zip = @"C:\Praktyki\zip.zip";
+        //string end = @"C:\Praktyki\end";
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if(checkBox1.Checked == false)
+            {
+                CommonOpenFileDialog fileDialog = new CommonOpenFileDialog();
+                fileDialog.InitialDirectory = @"C:\\";
+                fileDialog.IsFolderPicker = true;
+                if (fileDialog.ShowDialog() == CommonFileDialogResult.Ok)
+                {
+                    //MessageBox.Show("You selected: " + fileDialog.FileName);
+                    //string fileName;
+                    //fileName = fileDialog.FileName;
+                    string location = @fileDialog.FileName;
+                    if (File.Exists(location + ".zip"))
+                        File.Delete(location + ".zip");
+                    ZipFile.CreateFromDirectory(location, location + ".zip");
+                }
+            }
+            else
+            {
+                OpenFileDialog fileDialog = new OpenFileDialog();
+                fileDialog.Title = "Select File!";
+                fileDialog.InitialDirectory = @"c:\\";
+                fileDialog.Filter = "zip files (*.zip)|*.zip";
+                if (fileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    //MessageBox.Show("Extract function");
+                    FileInfo fi = new FileInfo(fileDialog.FileName);
+                    string direc = fi.DirectoryName;
+                    direc += @"\extracted "+fi.Name;
+                    ZipFile.ExtractToDirectory(fileDialog.FileName, direc);
+                    MessageBox.Show("Extracted to: " + direc);
+                }
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked == false)
+                button3.Text = "Open";
+            else
+                button3.Text = "Extract";
+        }
+    }
+}
---
+++ ZIP/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Ogólne informacje o zestawie są kontrolowane poprzez następujący 
+// zestaw atrybutów. Zmień wartości tych atrybutów, aby zmodyfikować informacje
+// powiązane z zestawem.
+[assembly: AssemblyTitle("ZIP")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ZIP")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Ustawienie elementu ComVisible na wartość false sprawia, że typy w tym zestawie są niewidoczne
+// dla składników COM. Jeśli potrzebny jest dostęp do typu w tym zestawie z
+// COM, ustaw wartość true dla atrybutu ComVisible tego typu.
+[assembly: ComVisible(false)]
+
+// Następujący identyfikator GUID jest identyfikatorem biblioteki typów w przypadku udostępnienia tego projektu w modelu COM
+[assembly: Guid("0d6d8ffa-4be0-4ff4-8edc-116bcecba680")]
+
+// Informacje o wersji zestawu zawierają następujące cztery wartości:
+//
+//      Wersja główna
+//      Wersja pomocnicza
+//      Numer kompilacji
+//      Poprawka
+//
+// Możesz określić wszystkie wartości lub użyć domyślnych numerów kompilacji i poprawki
+// przy użyciu symbolu „*”, tak jak pokazano poniżej:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
